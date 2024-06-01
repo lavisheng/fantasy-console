315f4305c8a3019e6a7c0649b976fa04159d89fc
lavisheng <hwangcodes@gmail.com>
2024-05-31 20:02:09 -0400

    Condition testing and condition move instructions

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,37 +1,46 @@
-use std::collections::HashMap;
 use crate::bus::Bus;
+use std::collections::HashMap;
 //use crate::cpu::Instructions;
 use crate::cpu::instructions;
 
-pub struct CPU{
+pub struct CPU {
     pub pc: u32,
     pb: u32, // program break location
     pub sp: u32,
-    pub r: [u32; 32],//registers
-    //pub status: u8,
+    pub r: [u32; 32], //registers
     bus: Bus,
     reg: HashMap<u32, fn(&mut CPU, (usize, usize, usize, usize))>, // 5 + 5 + 5 + 5
-    imm: HashMap<u32, fn(&mut CPU, (usize, usize, u16))>, // 5 + 5 + 16
-    jmp: HashMap<u32, fn(&mut CPU, u32)> // 6 + 26
+    imm: HashMap<u32, fn(&mut CPU, (usize, usize, u16))>,          // 5 + 5 + 16
+    jmp: HashMap<u32, fn(&mut CPU, u32)>,                          // 6 + 26
 }
 
-impl CPU{
+impl CPU {
     // size indicates program size(lines of code)
-    pub fn init(size: u32) -> CPU{
-        let r: HashMap<u32, fn(&mut CPU, (usize, usize, usize, usize))> = HashMap::from([
-            (0b100000, instructions::add as fn(&mut CPU, (usize, usize, usize, usize)))
-        ]);
+    pub fn init(size: u32) -> CPU {
+        let r: HashMap<u32, fn(&mut CPU, (usize, usize, usize, usize))> = HashMap::from([(
+            0b100000,
+            instructions::add as fn(&mut CPU, (usize, usize, usize, usize)),
+        )]);
         let i: HashMap<u32, fn(&mut CPU, (usize, usize, u16))> = HashMap::from([]);
         let j: HashMap<u32, fn(&mut CPU, u32)> = HashMap::from([]);
-        CPU{pc:0, pb: size, sp:0xffff-1, r: [0; 32], bus: Bus::init(), reg: r, imm: i, jmp: j}
+        CPU {
+            pc: 0,
+            pb: size,
+            sp: 0xffff - 1,
+            r: [0; 32],
+            bus: Bus::init(),
+            reg: r,
+            imm: i,
+            jmp: j,
+        }
     }
-    pub fn write_bus(&mut self, addr: u32, val:u32){
+    pub fn write_bus(&mut self, addr: u32, val: u32) {
         self.bus.write(addr, val);
     }
-    pub fn read_bus(&self, addr: u32) -> u32{
+    pub fn read_bus(&self, addr: u32) -> u32 {
         self.bus.read(addr)
     }
-    pub fn exec_inst(&mut self){
+    pub fn exec_inst(&mut self) {
         let bytecode = self.read_bus(self.pc);
         let op = bytecode >> 26; // most sig 6 bits are op code
         match op {
@@ -50,44 +59,46 @@ impl CPU{
                 // because it would run into issue with the loader
                 // left in as debugging for func testing though
                 match inst {
-                    Some(inst) => inst( self, (rs, rd, rt, shamt)),
+                    Some(inst) => inst(self, (rs, rd, rt, shamt)),
                     None => {
                         println!("invalid op!");
                     }
                 }
-            },
+            }
             1..=7 => {
                 let inst = self.jmp.get(&op).map(|x| *x);
                 // i think this works. This should be first 26 bits.
                 let addr = bytecode & 0x3FFFFFF;
                 match inst {
                     Some(inst) => inst(self, addr),
-                    None => println!("invalid op!")
+                    None => println!("invalid op!"),
                 }
-            },// jmp,
+            } // jmp,
             8..=43 => {
                 let rs = ((bytecode >> 21) & 0x1F) as usize;
                 let rd = ((bytecode >> 16) & 0x1F) as usize;
                 let imm = ((bytecode >> 6) & 0xFFFF) as u16;
                 let inst = self.imm.get(&op).map(|x| *x);
 
-                match inst{
+                match inst {
                     Some(inst) => inst(self, (rs, rd, imm)),
                     None => println!("invalid op!"),
                 }
-            },//imm
-            _ => {println!("{}: Not a valid op!", op)}
+            } //imm
+            _ => {
+                println!("{}: Not a valid op!", op)
+            }
         }
         // increment program counter
-        self.pc+=4;
+        self.pc += 4;
     }
 }
 
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn test_single_reg(){
+    fn test_single_reg() {
         let mut c = CPU::init(1);
         c.write_bus(c.pc, 0b000000_00000_00001_00010_00000_100000);
         c.r[1] = 1u32;
--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -4,40 +4,43 @@ use crate::cpu::cpu;
 
 //arithmetic
 // signed addition
-pub fn add(c: & mut cpu::CPU, data: (usize, usize, usize, usize)){
-    unsafe{
+pub fn add(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    unsafe {
         c.r[data.0] = std::mem::transmute::<i32, u32>(
-            std::mem::transmute::<u32, i32>(c.r[data.1]) +
-                std::mem::transmute::<u32, i32>(c.r[data.2]));
+            std::mem::transmute::<u32, i32>(c.r[data.1])
+                + std::mem::transmute::<u32, i32>(c.r[data.2]),
+        );
     }
 }
 
-//signed immediate addiiton
-pub fn addi(c: &mut cpu::CPU, data:(usize, usize, u16)){
+//signed immediate addition
+pub fn addi(c: &mut cpu::CPU, data: (usize, usize, u16)) {
     c.r[data.0] = ((c.r[data.1] as i32) + (data.2 as i32)) as u32;
 }
 
 //unsigned addition
-pub fn addu(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn addu(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] + c.r[data.2];
 }
 
 // unsigned immediate addition
-pub fn addiu(c: &mut cpu::CPU, data: (usize, usize, u16)){
+pub fn addiu(c: &mut cpu::CPU, data: (usize, usize, u16)) {
     c.r[data.0] = c.r[data.1] + data.2 as u32;
 }
 
 // count leading ones
-pub fn clo(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn clo(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     let num = c.r[data.1];
-    c.r[data.0] = (0..32).fold(0, |acc, elem| acc + ((num >> elem) & 1));
+    c.r[data.0] =
+        (0..32).fold(0, |acc, elem| acc + ((num >> elem) & 1));
 }
 
 // count leading 0s
-pub fn clz(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn clz(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     let num = c.r[data.1];
     // basically just invert the counter for when we do clo
-    c.r[data.0] = (0..32).fold(0, |acc, elem| acc + !((num >> elem) & 1));
+    c.r[data.0] =
+        (0..32).fold(0, |acc, elem| acc + !((num >> elem) & 1));
 }
 
 // need to figure out how to convert this
@@ -54,109 +57,112 @@ pub fn clz(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
 //}
 
 // load upper immediate
-pub fn lui(c: &mut cpu::CPU, data: (usize, usize, u16)){
+pub fn lui(c: &mut cpu::CPU, data: (usize, usize, u16)) {
     c.r[data.0] = (data.2 as u32) << 16;
 }
 
 // moves source to destination
-pub fn mov(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn mov(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1];
 }
 
 // negate move unsigned
-pub fn negu(c: &mut cpu::CPU, data:(usize, usize, usize, usize)){
+pub fn negu(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = !c.r[data.1];
 }
 
 // sign extend byte
-pub fn seb(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn seb(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] & 0xFF;
 }
 
 // sign extend half word
-pub fn seh(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn seh(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] & 0xFFFF;
 }
 
 // subtraction
-pub fn sub(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
-    unsafe{
+pub fn sub(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    unsafe {
         c.r[data.0] = std::mem::transmute::<i32, u32>(
-            (c.r[data.1] as i32) - (c.r[data.2] as i32));
+            (c.r[data.1] as i32) - (c.r[data.2] as i32),
+        );
     }
 }
 
 // subtraction unsigned
-pub fn subu(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn subu(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] - c.r[data.2];
 }
 
 // rotate right
-pub fn rotr(c: &mut cpu::CPU, data:(usize, usize, usize, usize)){
-    c.r[data.0] = (c.r[data.1] << (32 - data.3 + 1)) | (c.r[data.1] >> data.3);
+pub fn rotr(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    c.r[data.0] =
+        (c.r[data.1] << (32 - data.3 + 1)) | (c.r[data.1] >> data.3);
 }
 
 // rotate right variable
-pub fn rotrv(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn rotrv(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     let rot = c.r[data.2] & 0x1F;
-    c.r[data.0] = (c.r[data.1] << (32 - rot + 1)) | (c.r[data.1] >> rot);
+    c.r[data.0] =
+        (c.r[data.1] << (32 - rot + 1)) | (c.r[data.1] >> rot);
 }
 
-// shift left 
-pub fn sll(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+// shift left
+pub fn sll(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] << data.3;
 }
 
 // shift left variable
-pub fn sllv(c: &mut cpu::CPU, data: (usize, usize, usize)){
+pub fn sllv(c: &mut cpu::CPU, data: (usize, usize, usize)) {
     let shift = c.r[data.2] & 0x1F;
     c.r[data.0] = c.r[data.1] << shift;
 }
 
 // shift right
-pub fn sra(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
-    unsafe{
+pub fn sra(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    unsafe {
         c.r[data.0] = std::mem::transmute::<i32, u32>(
-            std::mem::transmute::<u32, i32>(c.r[data.1])
-        ) >> data.3;
+            std::mem::transmute::<u32, i32>(c.r[data.1]) >> data.3,
+        );
     }
     c.r[data.0] = c.r[data.1] >> data.3;
 }
 
 // shift right variable
-pub fn srav(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn srav(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     let shift = c.r[data.2] & 0x1F;
-    unsafe{
+    unsafe {
         c.r[data.0] = std::mem::transmute::<i32, u32>(
-            std::mem::transmute::<u32, i32>(c.r[data.1])
-        ) >> shift;
+            std::mem::transmute::<u32, i32>(c.r[data.1]) >> shift,
+        );
     }
 }
 
 // shift right logical
-pub fn srl(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn srl(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     // default u32 shift is logical
     c.r[data.0] = c.r[data.1] >> data.3;
 }
 
 // shift right logical variable
-pub fn srlv(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn srlv(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     let shift = c.r[data.2] & 0x1F;
     c.r[data.0] = c.r[data.1] >> shift;
 }
 
 // AND
-pub fn and(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn and(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] & c.r[data.2];
 }
 
 // and immediate
-pub fn andi(c: &mut cpu::CPU, data: (usize, usize, u16)){
+pub fn andi(c: &mut cpu::CPU, data: (usize, usize, u16)) {
     c.r[data.0] = c.r[data.1] & (data.2 as u32);
 }
 
 // extract
-pub fn ext(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn ext(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     // TODO: test this one since its pretty weird
     // do i need to shift it back after?
     let mask = ((1 << data.3) - 1) << data.2;
@@ -164,46 +170,104 @@ pub fn ext(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
 }
 
 // insert
-pub fn ins(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
-    let mask = (1 << data.3) -1;
+pub fn ins(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    let mask = (1 << data.3) - 1;
     c.r[data.0] |= (c.r[data.1] & mask) << data.2;
 }
 
 // nor
-pub fn nor(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn nor(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = !(c.r[data.1] | c.r[data.2]);
 }
 
 // NOT
-pub fn not(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn not(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = !c.r[data.1];
 }
 
 // or
-pub fn or(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn or(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] | c.r[data.2];
 }
 
-// ori 
-pub fn ori(c: &mut cpu::CPU, data: (usize, usize, u16)){
+// ori
+pub fn ori(c: &mut cpu::CPU, data: (usize, usize, u16)) {
     c.r[data.0] = c.r[data.1] | (data.2 as u32);
 }
 
+// word swap byte half
+pub fn wsbh(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    let mask = 0b1111_1111;
+    let source = c.r[data.1];
+    c.r[data.0] = ((source & (mask << 16)) << 8)
+        | ((source & (mask << 24)) >> 8)
+        | ((source & mask) << 8)
+        | ((source & mask << 8) >> 8);
+}
+
 // xor
-pub fn xor(c: &mut cpu::CPU, data: (usize, usize, usize, usize)){
+pub fn xor(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
     c.r[data.0] = c.r[data.1] ^ c.r[data.2];
 }
 
 // xori
-pub fn xori(c: &mut cpu::CPU, data: (usize, usize, u16)){
+pub fn xori(c: &mut cpu::CPU, data: (usize, usize, u16)) {
     c.r[data.0] = c.r[data.1] ^ (data.2 as u32);
 }
 
-mod tests{
+// movn
+pub fn movn(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    if data.2 != 0 {
+        c.r[data.0] = c.r[data.1];
+    }
+}
+
+// movz
+pub fn movz(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    if data.2 == 0 {
+        c.r[data.0] = c.r[data.1];
+    }
+}
+
+// slt
+pub fn slt(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    unsafe {
+        c.r[data.0] = if std::mem::transmute::<u32, i32>(c.r[data.1])
+            < std::mem::transmute::<u32, i32>(c.r[data.2])
+        {
+            1
+        } else {
+            0
+        };
+    }
+}
+
+pub fn slti(c: &mut cpu::CPU, data: (usize, usize, u16)) {
+    unsafe {
+        c.r[data.0] =
+            if std::mem::transmute::<usize, i32>(c.r[data.1])
+                < std::mem::transmute::<u16, i32>(data.2)
+            {
+                1
+            } else {
+                0
+            };
+    }
+}
+
+pub fn sltiu(c: &mut cpu::CPU, data: (usize, usize, u16)) {
+    c.r[data.0] = if c.r[data.1] < data.2 { 1 } else { 0 };
+}
+
+pub fn sltu(c: &mut cpu::CPU, data: (usize, usize, usize, usize)) {
+    c.r[data.0] = if c.r[data.1] < c.r[data.2] { 1 } else { 0 };
+}
+
+mod tests {
     use super::*;
 
     #[test]
-    fn test_ext(){
+    fn test_ext() {
         // am i interpreting this instruction correctly?
         let mut c = cpu::CPU::init(1);
         c.r[1] = 0b11011100000000000000000000000000;
@@ -212,7 +276,7 @@ mod tests{
     }
 
     #[test]
-    fn test_ins(){
+    fn test_ins() {
         let mut c = cpu::CPU::init(1);
         c.r[0] = 0;
         c.r[1] = 0b1111;
